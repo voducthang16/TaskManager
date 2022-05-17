8028592823c95c6e8d2c79748661b2891d42d668
GB <voducthang16@gmail.com>
2022-05-17 10:15:05 +0700

    create UI register page
    17.05.2022 - 10:15AM

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: RegisterComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        data: {
+            title: 'Đăng ký'
+        }
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+    constructor (private router: Router, private activatedRoute: ActivatedRoute, private title: Title) {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            map(() => {
+                let child = this.activatedRoute.firstChild;
+                while (child) {
+                    if (child.firstChild) {
+                        child = child.firstChild;
+                    } else if (child.snapshot.data && child.snapshot.data['title']) {
+                        return child.snapshot.data['title'];
+                    } else {
+                        return null;
+                    }
+                }
+                return null;
+            })
+        ).subscribe((data: any) => {
+            if (data) {
+                this.title.setTitle(data + ' - Vo Duc Thang');
+            }
+        });
+    }
 }
