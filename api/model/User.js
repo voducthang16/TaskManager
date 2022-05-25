c888b58612ccf4f5aea14fecfd45b29e0c0356cf
GB <voducthang16@gmail.com>
2022-05-25 08:56:14 +0700

    create Project model
    25.05.2022 - 9:00AM

--- api/model/User.js
+++ api/model/User.js
@@ -2,12 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const User = new Schema({
-    firstName: {type: String, require: true},
-    lastName: {type: String, require: true},
-    email: {type: String, require: true},
-    password: {type: String, require: true},
-    area: {type: String, require: true},
-    role: {type: Number, default: 2},
+    firstName: { type: String, require: true },
+    lastName: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    area: { type: String, require: true },
+    role: { type: Number, default: 2 },
+    status: { type: Number, default: 0 },
 }, {
     timestamps: true,
 })
