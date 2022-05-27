03ca9f9cf7ce971edf41a179b48917c45507f40b
GB <voducthang16@gmail.com>
2022-05-27 20:44:01 +0700

    upgrade user info
    27.05.2022 - 8:45PM

--- frontend/src/app/components/users/info-user/info-user.component.ts
+++ frontend/src/app/components/users/info-user/info-user.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Users } from 'src/app/interfaces/users';
 import { UsersService } from 'src/app/services/users.service';
 import { LoginComponent } from 'src/app/pages/login/login.component';
+import { AppComponent } from 'src/app/app.component';
+import { Areas } from 'src/app/interfaces/areas';
 @Component({
     selector: 'app-info-user',
     templateUrl: './info-user.component.html',
     styleUrls: ['./info-user.component.scss'],
-    providers: [LoginComponent]
+    providers: [LoginComponent, AppComponent]
 })
 export class InfoUserComponent implements OnInit {
     constructor(
         private UsersService: UsersService, 
         private user: LoginComponent,
+        private AppComponent: AppComponent
     ) {}
     userType: any = {};
     userInfo: Users  = {
@@ -24,6 +27,7 @@ export class InfoUserComponent implements OnInit {
         role: 0,
         status: 0
     };
+    listArea: Areas[] = []
     area(value: string) {
         if (value == 'south') {
             return 'Nam';
@@ -47,5 +51,6 @@ export class InfoUserComponent implements OnInit {
         this.user.userType.subscribe(value => this.userType = value);
         this.id = this.userType.id;
         this.UsersService.getOneUser(this.id).subscribe((user: any) => this.userInfo = user[0]);
+        this.listArea = this.AppComponent.listAreas;
     }
 }
