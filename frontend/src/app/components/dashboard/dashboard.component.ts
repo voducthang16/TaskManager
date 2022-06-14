afa58643112aa63327685f4f311c7ce21fb10fa0
GB <voducthang16@gmail.com>
2022-06-14 10:45:51 +0700

    update admin dashboard
    14.06.2022 - 10:45AM

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProjectsService } from 'src/app/services/projects.service';
+import { TasksService } from 'src/app/services/tasks.service';
+import { UsersService } from 'src/app/services/users.service';
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    constructor(
+        private ProjectsService: ProjectsService,
+        private TasksService: TasksService,
+        private UsersService: UsersService
+    ) {}
+    projectQuantity: number | undefined;
+    taskQuantity: number | undefined;
+    userQuantity: number | undefined;
+    totalMoney: number = 0;
+    ngOnInit(): void {
+        this.ProjectsService.getAllProject().subscribe((list: any) => {
+            this.projectQuantity = list.length;
+            list.forEach((project: any) => {
+                this.totalMoney += +project.price;
+            })
+        })
+        this.TasksService.getAllTasks().subscribe((list: any) => {
+            this.taskQuantity = list.length;
+        })
+        this.UsersService.getUserList().subscribe((list: any) => {
+            this.userQuantity = list.length;
+        })
+    }
 }
